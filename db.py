73366de3623f3b6bfd3b849461687db1f0ed0353
David-Roklem <roklem@mail.ru>
2023-07-03 16:45:43 +0300

    Clean db.py

--- db.py
+++ db.py
@@ -1,18 +1,3 @@
-import asyncio
-import asyncpg
-from config import DB_NAME, DB_USER, DB_PASSW, DB_HOST, DB_PORT
-
-
-async def establish_connection():
-    connection = await asyncpg.connect(
-            user=DB_USER,
-            password=DB_PASSW,
-            database=DB_NAME,
-            host=DB_HOST,
-            port=DB_PORT
-        )
-
-
 async def create_table(connection):
     await connection.execute('''
         CREATE TABLE IF NOT EXISTS vip_users (
