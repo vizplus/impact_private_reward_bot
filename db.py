f2eea1b674f753f880258895fa35f1785755c0b5
David-Roklem <roklem@mail.ru>
2023-07-05 13:20:54 +0300

    Point where bot works!
    At this point bot does:
    It all starts with the fact that the VIP enters the bot,
    sees the description. Next, he is invited to start the
    interaction with the start command, after which the bot
    asks to enter the name, regular key and reward size.
    This data is stored in the database. Now the VIP can
    forward the message, thereby rewarding the specified
    reward size to the user whose message he forwarded.
    In addition, the VIP can re-enter his data, including
    the regular key name and reward size. This is done by
    the edit command, which deletes the entry under the
    corresponding TG id, after which the bot offers to
    refill all the data by running the start command.
    Accordingly, one VIP with the given TG id can have
    only one entry in the database with the given settings
    for the regular key name and reward size.

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ async def create_table(connection):
     await connection.execute('''
         CREATE TABLE IF NOT EXISTS vip_users (
         id serial PRIMARY KEY,
-        tg_id integer,
+        tg_id bigint UNIQUE,
         viz_account name,
         regular_key text,
         reward_size numeric
--- main.py
+++ main.py
@@ -21,10 +21,55 @@ dp = Dispatcher(bot, storage=storage)
 
 @dp.message_handler(commands=['start'])
 async def handle_start_command(message: types.Message):
+    user_id = message.from_user.id
+
+    connection = await asyncpg.connect(
+            user=DB_USER,
+            password=DB_PASSW,
+            database=DB_NAME,
+            host=DB_HOST,
+            port=DB_PORT
+        )
+    await create_table(connection)
+
+    tg_id = await connection.fetchval('''
+    SELECT tg_id FROM vip_users WHERE tg_id = $1;
+    ''', user_id)
+
+    await connection.close()
+
+    if tg_id is None:
+        await message.answer(
+            'Hello! Please, write your name:'
+        )
+        await FSMIntro.Q_name.set()
+    else:
+        await message.answer(
+            'Nice try! There is already data under your Telegram id. '
+            'You can edit your data using /edit command.'
+        )
+
+
+@dp.message_handler(commands=['edit'])
+async def handle_edit_command(message: types.Message):
+    user_id = message.from_user.id
+
+    connection = await asyncpg.connect(
+            user=DB_USER,
+            password=DB_PASSW,
+            database=DB_NAME,
+            host=DB_HOST,
+            port=DB_PORT
+        )
+
+    await connection.execute('''
+    DELETE FROM vip_users WHERE tg_id = $1;
+    ''', user_id)
+
     await message.answer(
-        'Hello! Please, write your name:'
-    )
-    await FSMIntro.Q_name.set()
+        'OK, you deleted your data. '
+        'Now rewrite your data using /start command'
+        )
 
 
 @dp.message_handler(state=FSMIntro.Q_name)
@@ -44,7 +89,7 @@ async def handle_fsm_reg_key(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['reg_key'] = answer
     await message.answer(
-        'And lastly set the reward size for the user'
+        'And lastly set the reward size for the user '
         'you want to gift it with (minimum 1 VIZ):'
     )
     await FSMIntro.next()
@@ -72,13 +117,11 @@ async def handle_fsm_reward_size(message: types.Message, state: FSMContext):
         port=DB_PORT
     )
 
-    await create_table(connection)
-
     await connection.execute('''
         INSERT INTO vip_users (
         tg_id, viz_account, regular_key, reward_size
         )
-        VALUES ($1, $2, $3::text, $4);
+        VALUES ($1, $2, $3, $4);
     ''', tg_id, name, reg_key, reward_size)
 
     await connection.close()
@@ -89,7 +132,7 @@ async def handle_fsm_reward_size(message: types.Message, state: FSMContext):
 
 
 @dp.message_handler()
-async def echo(message: types.Message):
+async def handle_forwarded_msgs(message: types.Message):
     user_id = message.from_user.id
     author_id = message.forward_from.id if message.forward_from\
         else message.from_user.id
@@ -106,15 +149,25 @@ async def echo(message: types.Message):
         )
 
         reward_size = await connection.fetchrow('''
-            SELECT reward_size from vip_users
-            WHERE tg_id = $1 ORDER BY id DESC LIMIT 1;
+            SELECT reward_size from vip_users WHERE tg_id = $1;
         ''', user_id)
 
-        await message.answer(f'You rewarded a user under {author_id} TG-ID\n'
-                             f'with {reward_size[0]} VIZ')
+        await connection.close()
+
+        if reward_size:
+            await message.answer(
+                f'You rewarded a user under Telegram id {author_id}\n'
+                f'with {reward_size[0]} VIZ'
+            )
+        else:
+            await message.answer(
+                'You did not provide required data for me '
+                'to handle forwarded messages. '
+                'Use /start command to fix that.'
+            )
 
     else:
-        await message.answer('Please, end forwarded message, not your own!')
+        await message.answer('Please, send forwarded message, not your own!')
 
 
 if __name__ == '__main__':
